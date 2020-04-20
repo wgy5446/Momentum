68abeb1714a3a47a41ab97f762047ab31cd510c6
wgy5446 <wjy5446@naver.com>
2020-04-20 11:54:27 +0900

    feat: test

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,17 +1,17 @@
 // fadeIn
 const fadeIn = (target, duration) => {
-  console.log(‘fadeIn’);
+  console.log('fadeIn');
   target.style.animationDuration = `${duration / 1000}s`;
-  target.classList.add(‘fade-in’);
-  target.classList.remove(‘fade-out’);
+  target.classList.add('fade-in');
+  target.classList.remove('fade-out');
 };
 // fadeOut
 const fadeOut = (target, duration) => {
   target.style.animationDuration = `${duration / 1000}s`;
-  target.classList.add(‘fade-out’);
-  target.classList.remove(‘fade-in’);
+  target.classList.add('fade-out');
+  target.classList.remove('fade-in');
   setTimeout(() => {
-    target.classList.remove(‘fade-out’);
+    target.classList.remove('fade-out');
   }, duration);
 };
 export { fadeIn, fadeOut };
